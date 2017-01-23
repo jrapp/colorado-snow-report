3c5b15e309ac792a1a28ed4b205d60cae744c4f9
Ubuntu <ubuntu@ip-172-31-34-87.us-west-2.compute.internal>
2017-01-23 05:57:30 +0000

    started adding A-Basin, struggling with finding the html

--- harvest.py
+++ harvest.py
@@ -5,6 +5,7 @@ from datetime import date
 import time
 import boto3
 import logging
+import sys
 
 class ColoradoSnowReport:
     def __init__(self):
@@ -33,23 +34,53 @@ class ColoradoSnowReport:
         logging.debug('Posted item: ' + date.today().isoformat() + ', ' + resort + ', ' + reporting) 
 
     def Breckenridge(self):
-        self.driver.get('http://www.breckenridge.com/mountain/snow-and-weather-report.aspx')
-        temp = self.driver.find_element_by_class_name('snowReportPage')
-        temp = temp.find_element_by_class_name('snowBorder')
-        temp = temp.find_element_by_id('snowReport')
-        temp = temp.find_element_by_class_name('snowReportData')
-        temp = temp.find_element_by_class_name('snowReportDataColumn1')
-        temp = temp.find_element_by_class_name('snowfallData')
-        temp = temp.text.split(' ')
-        logging.debug('Breck reported' + temp[0])
-        self.post_to_table('Breckenridge', temp[0])
+        snowfall = 'nr'
+        try:
+            self.driver.get('http://www.breckenridge.com/mountain/snow-and-weather-report.aspx')
+            temp = self.driver.find_element_by_class_name('snowReportPage')
+            temp = temp.find_element_by_class_name('snowBorder')
+            temp = temp.find_element_by_id('snowReport')
+            temp = temp.find_element_by_class_name('snowReportData')
+            temp = temp.find_element_by_class_name('snowReportDataColumn1')
+            temp = temp.find_element_by_class_name('snowfallData')
+            temp = temp.text.split(' ')
+            logging.debug('Breck reported' + temp[0])
+            snowfall = temp[0]
+        except:
+            logging.error('Error obtaining Breckenridge data')
+            snowfall = 'error'
+        try:
+            self.post_to_table('Breckenridge', snowfall)
+        except:
+            logging.error('could not post data to dynamodb')
+
         logging.debug('Posted Breckenridge data to Snow Report Table')
+
+    def A_Basin(self):
+        snowfall = 'nr'
+        try:
+            self.driver.get('http://arapahoebasin.com/ABasin/snow-conditions/')
+            temp = self.driver.find_element_by_class_name('columns')
+            print(temp)
+            temp = temp.find_element_by_class_name('page-image')
+            print('2')
+            temp = temp.find_element_by_id('mountain-conditions')
+            print('3')
+            temp = temp.find_element_by_class_name('nine')
+            print('4')
+            temp = temp.find_elements_by_tag_name('li')
+            print('5')
+            temp = temp[0]
+            print(temp)
+        except:
+            print(sys.exc_info())
 
 if __name__ == "__main__":
     #Call the constructor
     csr = ColoradoSnowReport()
     #Call the functions for the individual resorts
-    csr.Breckenridge()
+    #csr.Breckenridge()
+    csr.A_Basin()
     csr.driver.close()
 
 
